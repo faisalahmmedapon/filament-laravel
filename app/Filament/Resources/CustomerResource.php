64f10d8d477edb520465f04cabe6ede772d0cba1
Faisal Ahmmed Apon <86137950+faisalahmmedapon@users.noreply.github.com>
2023-10-12 11:18:18 +0600

    add filament admin with customer full curd
    add filament admin with customer full curd

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomerResource\Pages;
+use App\Filament\Resources\CustomerResource\RelationManagers;
+use App\Models\Customer;
+use Faker\Core\Number;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                ->required()->unique()
+                ->maxLength(255),
+                TextInput::make('email')
+                ->required()->unique()
+                ->maxLength(255),
+                TextInput::make('phone')
+                ->required()->unique()
+                ->maxLength(255),
+                FileUpload::make('image'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Name')->searchable(isIndividual: true)->limit(5),
+                TextColumn::make('email')->label('Email')->searchable(),
+                TextColumn::make('phone')->label('Phone')->searchable(),
+                ImageColumn::make('image')->label('Photo'),
+            ])
+            ->filters([
+
+                // Filter::make('is_featured')->label('Featured'),
+
+                // SelectFilter::make('status')
+                // ->options([
+                //     'draft' => 'Draft',
+                //     'reviewing' => 'Reviewing',
+                //     'published' => 'Published',
+                // ])
+
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
